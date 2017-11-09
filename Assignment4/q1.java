32e01d013572fbcd884703b4bd716397e78dd034
vikash kumar <iit2016041@iiita.ac.in>
2017-11-09 15:32:09 +0530

    initial commit

---
+++ Assignment4/q1.java
@@ -0,0 +1,13 @@
+//import for Scanner and other utility classes
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();                 // Reading input from STDIN
+        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+        
+
+    }
+}
